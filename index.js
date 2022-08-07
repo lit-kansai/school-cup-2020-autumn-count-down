86a92fde0a15d6b0c01209777aad5ab522ca4d3e
Ritsuki Goto <develop@mizucoffee.net>
2022-08-07 09:56:13 +0900

    [update] for 2022 spring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = () => {
   const message = document.querySelector("#message")
   const countdown = document.querySelector("#countdown")
-  const deadline = dayjs('2021-02-21 16:30:00')
+  const deadline = dayjs('2022-08-07 16:30:00')
   
   const timer = () => {
     let diff = deadline.diff();
