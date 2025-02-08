ddfb55f22641e50c09dca8c591a4581c5208be82
Yo Higashida <y.higashida0226+app@gmail.com>
2025-02-08 18:22:12 +0900

    Update for 2024 autumn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = () => {
-  const message = document.querySelector("#message")
-  const countdown = document.querySelector("#countdown")
-  const deadline = dayjs('2022-08-07 16:30:00')
-  
+  const message = document.querySelector("#message");
+  const countdown = document.querySelector("#countdown");
+  const deadline = dayjs("2025-02-16 16:30:00");
+
   const timer = () => {
     let diff = deadline.diff();
-    if(diff > 0){
+    if (diff > 0) {
       const day = Math.floor(diff / 86400000);
       diff -= 86400000 * day;
       const hour = Math.floor(diff / 3600000);
@@ -16,14 +16,14 @@ window.onload = () => {
       diff -= 1000 * sec;
       countdown.innerText = `${zeroPad(day)}日 ${zeroPad(hour)}時間 ${zeroPad(min)}分 ${zeroPad(sec)}秒`;
     } else {
-      message.innerText = "作品提出期間は終了しました！"
-      countdown.innerText = "お疲れさまでした！"
+      message.innerText = "作品提出期間は終了しました！";
+      countdown.innerText = "お疲れさまでした！";
     }
-    setTimeout(timer,100)
-  }
-  timer()
-}
+    setTimeout(timer, 100);
+  };
+  timer();
+};
 
 function zeroPad(n) {
-  return ( '00' + n ).slice(-2)
+  return ("00" + n).slice(-2);
 }
